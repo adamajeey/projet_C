5a051f7fc1a9590282ca7d2366201fd7915cfc71
Adama Dieye <adamadieye780@gmail.com>
2022-07-26 10:18:31 +0000

    ceci est mon premier projet en c

---
+++ Projet2.c
@@ -0,0 +1,85 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    char NomElev[50];
+    float NoteMath[2], NoteAnglais[2], NotePc[2], NoteFrancais[2];
+    float Moy_math=0, Moy_anglais=0, Moy_pc=0, Moy_francais=0, MoyGenerale;
+    float Som_math=0,Som_anglais=0, Som_Pc=0, som_Francais=0;
+    int const coefMath=4, coefAnglais=2, coefPc=3, coefFrancais=4, coefTolal;
+    int i;
+
+        printf("Veillez entrez votre Nom svp: ");
+        scanf("%s", NomElev);
+    
+    for (int i = 0; i < 2; i++)
+    {
+        printf("Entez la note en math %d: ", i+1);
+        if (scanf("%f", &NoteMath[i]) ==0 || NoteMath[i]>20 || NoteMath[i]<0)
+        {
+            printf("La note doit etre un nombre entre 0 et 20 ");
+            return 0;  
+        }
+        Som_math=Som_math+NoteMath[i];
+    }
+        Moy_math=(Som_math*coefMath)/2;
+
+
+    for (int i = 0; i < 2; i++)
+    {
+        printf("Entrez la note en aglais %d: ", i+1);
+       if (scanf("%f", &NoteAnglais[i]) ==0 || NoteAnglais[i]>20 || NoteAnglais[i]<0)
+       {
+          printf("La note doit etre un nombre entre 0 et 20 ");
+          return 0;
+       }
+        Som_anglais=Som_anglais+NoteAnglais[i];
+    }
+        Moy_anglais=(Som_anglais*coefAnglais)/2;
+
+    for (int i = 0; i < 2; i++)
+    {
+        printf("Entrez le note en Pc %d: ", i+1);
+        if (scanf("%f", &NotePc[i]) ==0 || NotePc[i]>20 || NotePc[i]<0)
+        {
+            printf("La note doit etre un nombre entre 0 et 20 ");
+            return 0;  
+        }
+        Som_Pc=Som_Pc+NotePc[i];
+    }
+        Moy_pc=(Som_Pc*coefPc)/2;
+    
+    for (int i = 0; i < 2; i++)
+    {
+        printf("Entrez la note en francais %d: ", i+1);
+        if (scanf("%f", &NoteFrancais[i]) ==0 || NoteFrancais[i]>20 || NoteFrancais[i]<0)
+        {
+            printf("La note doit etre un nombre entre 0 et 20 ");
+            return 0;  
+        }
+        som_Francais=som_Francais+NoteFrancais[i];
+    }
+        Moy_francais=(som_Francais*coefFrancais)/2;
+
+    MoyGenerale=(Moy_math+Moy_anglais+Moy_pc+Moy_francais)/13;
+    printf("%s Votre moyenne est: %1.f\n", NomElev,MoyGenerale);
+
+    if (MoyGenerale<10)
+    {
+        printf("C'est Mediocre\n\n");
+    }
+    else if (MoyGenerale==10)
+    {
+        printf("C'est Passable\n\n");
+    }
+    else if (MoyGenerale>=11 && MoyGenerale<=14)
+    {
+        printf("C'est Très bien\n\n");
+    }
+    else
+    {
+        printf("C'est Excellent\n\n");
+    }
+    
+    return 0;
+}
---
+++ jeey.c
@@ -0,0 +1,73 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int verif(char reponse[25]);
+int main()
+{
+/*/Déclaration des variables/*/
+    int tab[3]={12345,1234,123};
+    char reponse[25];
+    int i=0;                        
+    int cpt=0;
+        printf("Veiller saisir votre code secret: ");  //Il demande à l'utilisateur d'entrer le code secré
+        scanf("%s", reponse);  //l'utilisateur va ecrire son code secré
+        while (verif(reponse)==0) /*/ Ici nous avons utilisé la boucle while qui va nous dire que tant que la fonction verif est fausse il va afficher veillez saisir un nombre/*/
+        {
+            printf("Veillez saisir un nombre: ");
+            scanf("%s", reponse);
+        }
+        
+    while (tab[i] != atoi(reponse)) /*/ Ici nous avons uliliser la boucle while qui veut tant que le caractère est différent de le reponse qu'on a converti en entier faire/*/
+    {
+        if (i<3) /*/ Ici nous avons la condition if qui va verifier le nombre de tentative pour dire d'il reste des essais ou bien le compte est bloqué/*/
+        {
+            i++; 
+        }
+        else{
+            cpt++;
+            i = 0;
+            if (cpt == 3)
+            {
+                printf("Fin des tentatives, compte bloque ");
+                return 0;
+            }
+            if (cpt == 2)
+            {
+                printf("il vous reste un essai: ");
+            }
+            else
+            {
+                printf("Le code secret est incorrect, veillez \nreessayer: ");
+            }
+            scanf("%s", reponse);
+    while (verif(reponse)==0)
+            {
+                printf("Veillez saisir un nombre: ");
+                scanf("%s", reponse);
+            }
+            
+        }  
+    }
+     printf("Le code secret saisi est correct");
+    return 0;
+}
+int verif(char reponse[25])
+{
+    int compteur=0;
+    for (int i = 0; i < strlen(reponse); i++)
+    {
+        if (reponse[i]>='0'&& reponse[i] <='9')
+        {
+            compteur++;
+        }
+        
+    }
+        if (compteur == strlen(reponse))
+        {
+            return 1;
+        }
+        else
+        {
+            return 0;
+        }
+}
